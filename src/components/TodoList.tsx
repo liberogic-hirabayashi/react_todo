2bdef619a653d88a094897f79bce9b2194b3abbb
平林真奈 <>
2024-04-22 17:12:55 +0900

    ステータス機能追加

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-type todoObject={ id: number; text: string; }[]
+type todoObject={ id: number; text: string; status:string }[]
 
 function TodoList() {
   const [todos, setTodos] = useState<todoObject>([]);
   const [text, setText] = useState<string>("");
   const [todoId, setTodoId] = useState<number>(todos.length + 1);
+  const [status,setStatus]=useState('Not started')
 
   const addTodo = () => {
     if (text !== "") {
-      setTodos([...todos, { id: todoId, text: text }]);
+      setTodos([...todos, { id: todoId, text: text,status:status }]);
       setTodoId(todoId + 1);
       setText("");
+      console.log(todos)
     }
+
   };
 
   const removeTodo = (i) => {
@@ -21,17 +24,30 @@ function TodoList() {
   const todoValue = (e) => {
     setText(e.target.value);
   };
+  const todoStatus=(e)=>{
+    const value=e.target.value
+    setStatus(value)
+  }
+
 
   return (
     <>
       <div>
         <input type="text" value={text} onChange={todoValue} />
+        
         <button onClick={() => addTodo()}>追加</button>
       </div>
+      
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            {todo.text}
+            {todo.text}：
+            {todo.status}
+            <select name="status" onChange={()=>todoStatus}>
+          <option value="Not started">Not started</option>
+          <option value="In progress">In progress</option>
+          <option value="Done">Done</option>
+        </select>
             <button onClick={() => removeTodo(todo)} className="remove">
               削除
             </button>
