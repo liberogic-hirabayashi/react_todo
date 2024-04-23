bd5657eb3b4aaaf9c7b9844b142b3c13d34c7a91
平林真奈 <>
2024-04-23 16:25:20 +0900

    編集機能追加

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
-type todoObject={ id: number; text: string; status:string }[]
+type todoObject = { id: number; text: string; status: string }[];
 
 function TodoList() {
   const [todos, setTodos] = useState<todoObject>([]);
   const [text, setText] = useState<string>("");
   const [todoId, setTodoId] = useState<number>(todos.length + 1);
-  const [status,setStatus]=useState('Not started')
+  const [status, setStatus] = useState<string>("Not started");
+  const [edit, setEdit] = useState<boolean>(true);
+  const [editId, seteditId] = useState<number | null>(null);
+  const [editText, setEditText] = useState<string>("");
 
   const addTodo = () => {
     if (text !== "") {
-      setTodos([...todos, { id: todoId, text: text,status:status }]);
+      setTodos([...todos, { id: todoId, text: text, status: status }]);
       setTodoId(todoId + 1);
       setText("");
-      console.log(todos)
     }
-
   };
 
   const removeTodo = (i) => {
@@ -24,31 +25,60 @@ function TodoList() {
   const todoValue = (e) => {
     setText(e.target.value);
   };
-  const todoStatus=(e)=>{
-    const value=e.target.value
-    setStatus(value)
-  }
 
+  const editValue = (e) => {
+    setEditText(e.target.value);
+  };
+
+  const editTodo = (todo) => {
+    setEdit(false);
+    setEditText(todo.text);
+    seteditId(todo.id);
+  };
+
+  const editSave = () => {
+    const newArray = todos.map((todo) => {
+     return todo.id === editId ? { ...todo, text: editText } : todo;
+    });
+    console.log(newArray)
+    setTodos(newArray);
+    cancel();
+  };
+
+  const cancel = () => {
+    setEdit(true);
+    setText("");
+    seteditId(null);
+  };
 
   return (
     <>
-      <div>
-        <input type="text" value={text} onChange={todoValue} />
-        
-        <button onClick={() => addTodo()}>追加</button>
-      </div>
-      
+      {edit ? (
+        <div>
+          <input type="text" value={text} onChange={todoValue} />
+          <button onClick={() => addTodo()}>追加</button>
+        </div>
+      ) : (
+        <div>
+          <input type="text" value={editText} onChange={editValue} />
+          <button onClick={() => editSave()}>編集を保存</button>
+          <button onClick={() => cancel()}>キャンセル</button>
+        </div>
+      )}
+
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            {todo.text}：
-            {todo.status}
-            <select name="status" onChange={()=>todoStatus}>
+            {todo.text}
+            {/* <select name="status" >
           <option value="Not started">Not started</option>
           <option value="In progress">In progress</option>
           <option value="Done">Done</option>
-        </select>
-            <button onClick={() => removeTodo(todo)} className="remove">
+        </select> */}
+            <button className="sm-btn" onClick={() => editTodo(todo)}>
+              編集
+            </button>
+            <button onClick={() => removeTodo(todo)} className="sm-btn">
               削除
             </button>
           </li>
