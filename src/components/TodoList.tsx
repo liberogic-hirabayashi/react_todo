86c9cb6122114a0c5e2c47babac028f1b6c6bc74
平林真奈 <>
2024-04-24 15:29:58 +0900

    ステータスでの絞り込み実装

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,26 +1,51 @@
-import { useState } from "react";
-type todoObject = { id: number; text: string; status: string }[];
+import { useState, useEffect } from "react";
+type TodoObject = { id: number; text: string; status: string }[];
 
 function TodoList() {
-  const [todos, setTodos] = useState<todoObject>([]);
+  const [todos, setTodos] = useState<TodoObject>([]);
   const [text, setText] = useState<string>("");
   const [todoId, setTodoId] = useState<number>(todos.length + 1);
   const [status, setStatus] = useState<string>("Not started");
-  const [edit, setEdit] = useState<boolean>(true);
+  const [edit, setEdit] = useState<boolean>(false);
   const [editId, seteditId] = useState<number | null>(null);
   const [editText, setEditText] = useState<string>("");
+  const [filter, setFilter] = useState<string>("");
+  const [filteredTodos, setFilteredTodos] = useState<TodoObject>([]);
+
+  useEffect(() => {
+    const filteringTodos = () => {
+      switch (filter) {
+        case "Not started":
+          setFilteredTodos(
+            todos.filter((todo) => todo.status === "Not started")
+          );
+          break;
+        case "In progress":
+          setFilteredTodos(
+            todos.filter((todo) => todo.status === "In progress")
+          );
+          break;
+        case "Done":
+          setFilteredTodos(todos.filter((todo) => todo.status === "Done"));
+          break;
+        default:
+          setFilteredTodos(todos);
+      }
+    };
+    filteringTodos();
+  }, [filter, todos]);
 
   const addTodo = () => {
     if (text !== "") {
       setTodos([...todos, { id: todoId, text: text, status: status }]);
       setTodoId(todoId + 1);
       setText("");
+      // console.log(todos);
     }
   };
 
   const removeTodo = (i) => {
     setTodos(todos.filter((todo) => todo !== i));
-    console.log(todos);
   };
   const todoValue = (e) => {
     setText(e.target.value);
@@ -31,50 +56,80 @@ function TodoList() {
   };
 
   const editTodo = (todo) => {
-    setEdit(false);
+    setEdit(true);
     setEditText(todo.text);
     seteditId(todo.id);
   };
 
   const editSave = () => {
     const newArray = todos.map((todo) => {
-     return todo.id === editId ? { ...todo, text: editText } : todo;
+      return todo.id === editId ? { ...todo, text: editText } : todo;
     });
-    console.log(newArray)
     setTodos(newArray);
     cancel();
   };
 
   const cancel = () => {
-    setEdit(true);
+    setEdit(false);
     setText("");
     seteditId(null);
+  };
+
+  const changeStatus = (targetTodo, e) => {
+    // console.log(targetTodo)
+    const newArray = todos.map((todo) =>
+      todo.id === targetTodo.id ? { ...todo, status: e.target.value } : todo
+    );
+
+    setTodos(newArray);
+  };
+  const changeFilter = (e) => {
+    setFilter(e.target.value);
   };
 
   return (
     <>
       {edit ? (
         <div>
-          <input type="text" value={text} onChange={todoValue} />
-          <button onClick={() => addTodo()}>追加</button>
-        </div>
-      ) : (
-        <div>
           <input type="text" value={editText} onChange={editValue} />
           <button onClick={() => editSave()}>編集を保存</button>
           <button onClick={() => cancel()}>キャンセル</button>
         </div>
+      ) : (
+        <div>
+          <input type="text" value={text} onChange={todoValue} />
+          <button onClick={() => addTodo()}>追加</button>
+          <select name="status" value={filter} onChange={changeFilter}>
+            <option value="All">All</option>
+            <option value="Not started">Not started</option>
+            <option value="In progress">In progress</option>
+            <option value="Done">Done</option>
+          </select>
+        </div>
       )}
 
       <ul>
-        {todos.map((todo) => (
+        {filteredTodos.map((todo) => (
           <li key={todo.id}>
-            {todo.text}
-            {/* <select name="status" >
-          <option value="Not started">Not started</option>
-          <option value="In progress">In progress</option>
-          <option value="Done">Done</option>
-        </select> */}
+            {todo.status === "Not started" && <span>{todo.text}</span>}
+            {todo.status === "In progress" && (
+              <span className="inProgress">{todo.text}</span>
+            )}
+            {todo.status === "Done" && (
+              <span className="done">{todo.text}</span>
+            )}
+
+            <select
+              name="status"
+              value={todo.status}
+              onChange={(e) => {
+                changeStatus(todo, e);
+              }}
+            >
+              <option value="Not started">Not started</option>
+              <option value="In progress">In progress</option>
+              <option value="Done">Done</option>
+            </select>
             <button className="sm-btn" onClick={() => editTodo(todo)}>
               編集
             </button>
